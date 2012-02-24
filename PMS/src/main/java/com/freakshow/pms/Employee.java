cee273fe112f181d74fb38bbf9df2dded94fed76
Armin Tavazo <armint_@hotmail.com>
2012-02-24 12:55:20 -0800

    rev3

---
+++ PMS/src/main/java/com/freakshow/pms/Employee.java
@@ -0,0 +1,79 @@
+package com.freakshow.pms;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+/**
+ * The Class UserBean.
+ */
+@Entity
+@Table(name="employee")
+public class Employee implements Serializable {
+    
+    @Id
+    private int emp_ID; 
+    private String emp_pass;
+    private String first_name;
+    private String last_name;
+    private Date emp_start_date;
+    private String ft_pt;
+    private String supervisor_id;
+    private String ts_approver_id;
+    
+    public int getEmp_ID() {
+        return emp_ID;
+    }
+    public void setEmp_ID(int emp_ID) {
+        this.emp_ID = emp_ID;
+    }
+    public String getEmp_pass() {
+        return emp_pass;
+    }
+    public void setEmp_pass(String emp_pass) {
+        this.emp_pass = emp_pass;
+    }
+    public String getFirst_name() {
+        return first_name;
+    }
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+    public String getLast_name() {
+        return last_name;
+    }
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+    public Date getEmp_start_date() {
+        return emp_start_date;
+    }
+    public void setEmp_start_date(Date emp_start_date) {
+        this.emp_start_date = emp_start_date;
+    }
+    public String getFt_pt() {
+        return ft_pt;
+    }
+    public void setFt_pt(String ft_pt) {
+        this.ft_pt = ft_pt;
+    }
+    public String getSupervisor_id() {
+        return supervisor_id;
+    }
+    public void setSupervisor_id(String supervisor_id) {
+        this.supervisor_id = supervisor_id;
+    }
+    public String getTs_approver_id() {
+        return ts_approver_id;
+    }
+    public void setTs_approver_id(String ts_approver_id) {
+        this.ts_approver_id = ts_approver_id;
+    }
+    
+
+
+}
+
--- PMS/src/main/java/com/freakshow/pms/ManageUser.java
+++ PMS/src/main/java/com/freakshow/pms/ManageUser.java
@@ -1,30 +1,35 @@
 package com.freakshow.pms;
 
 import java.io.Serializable;
+
 import javax.ejb.Stateful;
-import javax.enterprise.context.RequestScoped;
-import javax.inject.Inject;
+import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-
+import javax.faces.event.ActionEvent;  
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;  
 /**
  * The Class ManageUser.
  */
 @Named("manageUser")
-@RequestScoped
+@SessionScoped
 @Stateful
 public class ManageUser implements Serializable {
     
-    @PersistenceContext 
+    @PersistenceContext
     protected EntityManager em;
-    @Inject
-    UserBean ub = new UserBean();
-    void persist(){
-        em.persist(ub);
-        em.flush();
+    Employee emp = new Employee();
+    
+    public Employee getUser() {
+        return emp;
     }
+
+    public void setUser(Employee user) {
+        this.emp = user;
+    }
+   
     public String verifyUser(){
         /*
         Query query = em.createQuery("from " +
@@ -32,5 +37,29 @@ public class ManageUser implements Serializable {
         ub = (UserBean)query.getSingleResult();
         */
         return "login";
-    }     
+    }   
+    
+    public void createUser() {
+
+        em.persist(emp);
+    }
+    public void save(ActionEvent actionEvent) {  
+        addMessage("Data saved");  
+    }  
+      
+    public void update(ActionEvent actionEvent) {  
+        addMessage("Data updated");  
+    }  
+      
+    public void delete(ActionEvent actionEvent) {  
+        addMessage("Data deleted");  
+    }  
+      
+    public void addMessage(String summary) {  
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary,  null);  
+        FacesContext.getCurrentInstance().addMessage(null, message);  
+    }    
+    public void addInfo(ActionEvent actionEvent) {  
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,"Welcome Employee", "You have been added to a new project!"));  
+    } 
 }
